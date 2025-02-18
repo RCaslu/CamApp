3018812d05a790cd123f65d4b2a78498469188c5
Rafael Caslu Henry <rafaelhenrycassiano23@gmail.com>
2025-02-18 00:38:02 -0300

    🔥 BREAKING CHANGE: Chan ged folders structure separating back and front

---
+++ front/src/app/index.tsx
@@ -0,0 +1,21 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text>Index</Text>
+      <View>
+      
+      </View>
+
+      <View>
+      
+    </View>
+
+    <View>
+      
+    </View>
+
+    </View>
+  );
+}
